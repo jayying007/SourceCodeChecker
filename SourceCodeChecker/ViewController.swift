8f46bcd81d62daf798ddca32f342ae57747d3271
jayying007 <1172510964@qq.com>
2023-11-04 11:21:12 +0800

    Initial Commit

---
+++ SourceCodeChecker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SourceCodeChecker
+//
+//  Created by janezhuang on 2023/11/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
